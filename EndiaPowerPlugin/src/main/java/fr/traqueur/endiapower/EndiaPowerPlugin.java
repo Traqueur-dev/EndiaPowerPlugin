5d92b11d23f82534670d31c241fd719356b41cbe
Traqueur-dev <traqueur.dev@gmail.com>
2024-05-07 11:41:08 +0200

    first commit

---
+++ EndiaPowerPlugin/src/main/java/fr/traqueur/endiapower/EndiaPowerPlugin.java
@@ -0,0 +1,16 @@
+package fr.traqueur.endiapower;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EndiaPowerPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
