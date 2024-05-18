c09ca8727ce805e0fd9ca41f8137eb1ce9f2c9c6
Traqueur-dev <traqueur.dev@gmail.com>
2024-05-18 22:17:34 +0200

    feat (api) : add javadoc to api

--- EndiaPowerAPI/src/main/java/fr/traqueur/endiapower/api/commands/arguments/Argument.java
+++ EndiaPowerAPI/src/main/java/fr/traqueur/endiapower/api/commands/arguments/Argument.java
@@ -2,4 +2,8 @@ package fr.traqueur.endiapower.api.commands.arguments;
 
 import java.util.List;
 
+/**
+ * The class Argument.
+ * <p> This class is used to represent an argument of a command. </p>
+ */
 public record Argument(String arg, List<String> completion) {}
